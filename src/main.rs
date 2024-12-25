4c05e64f6da457b7e948bebf6d62399c2c6939d6
Evan Chisholm <elchisholm248@gmail.com>
2024-12-25 00:35:05 -0500

    added color

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use minifb::{Key, Window, WindowOptions};
 const WIDTH: usize = 600;
 const HEIGHT: usize = 600;
 
+const MAX_ITER: u32 = 100;
 const COMP_WIDTH: f64 = 2.0;
 const COMP_HEIGHT: f64 = 2.0;
 
@@ -43,19 +44,32 @@ fn modulus(z: &Complex) -> f64 {
     (z.im * z.im + z.re * z.re).sqrt()
 }
 
-fn diverges(c: &Complex, n: u32) -> bool {
+fn mandlebrot(c: &Complex, n: u32) -> u32 {
     let mut z_n = Complex { re: c.re, im: c.im };
 
-    for _ in 0..n {
+    for i in 0..n {
         if modulus(&z_n) > 2.0 {
-            return true
+            return i 
         }
 
         let z_square = mul(&z_n, &z_n);
         z_n = add(&z_square, c);
     }
 
-    modulus(&z_n) > 2.0
+    n
+}
+
+fn map_to_color(iter: u32) -> (u8, u8, u8) {
+    if iter == MAX_ITER {
+        return (0, 0, 0);
+    }
+
+    let t = iter as f64 / MAX_ITER as f64;
+    let r = (t * 255.0) as u8;
+    let g = ((1.0 - t) * 255.0) as u8;
+    let b = 255 - r;
+
+    (r, g, b)
 }
 
 fn main() {
@@ -63,28 +77,6 @@ fn main() {
     let mut center_x = -0.5;
     let mut center_y  = 0.0;
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            // let c = window_to_complex(x, y);
-            let c = window_to_complex_plane(
-                x as f64,
-                y as f64,
-                WIDTH as f64,
-                HEIGHT as f64,
-                center_x - scope,
-                center_x + scope,
-                center_y -scope,
-                center_y + scope,
-            );
-            let point_diverges = diverges(&c, 100);
-            if point_diverges {
-                buffer[x + y * WIDTH] = 0xFF0000;
-            } else {
-                buffer[x + y * WIDTH] = 0;
-            }
-        }
-    }
 
     // Create a window
     let mut window = Window::new(
@@ -144,12 +136,12 @@ fn main() {
                     center_y -scope,
                     center_y + scope,
                 );
-                let point_diverges = diverges(&c, 100);
-                if point_diverges {
-                    buffer[x + y * WIDTH] = 0xFF0000;
-                } else {
-                    buffer[x + y * WIDTH] = 0;
-                }
+                let iter = mandlebrot(&c, 100);
+                let (r, g, b) = map_to_color(iter);
+                
+                let color = (r as u32) << 16 | (g as u32) << 8 | b as u32;
+
+                buffer[y * WIDTH + x] = color;
             }
         }
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
